747c42e10ed2c5cdc0f8c06083670b5683134431
PoinaritaDiana <61639111+PoinaritaDiana@users.noreply.github.com>
2020-05-10 20:37:01 +0300

    Add rotate & insertFix

--- set/Set.h
+++ set/Set.h
@@ -11,7 +11,10 @@ class Set {
     Comparator<T> cmp;
 
     void cleanup(Node<T>*);  
+
     void fixInsert(Node<T>*);
+    void leftRotate(Node<T>*); 
+    void rightRotate(Node<T>*);
 
 public:
     Set(); 
@@ -80,8 +83,94 @@ Set<T, F>::~Set(){
 }
 
 template<typename T, typename F>
-void Set<T, F>::fixInsert(Node<T>* n) {
+void Set<T, F>::leftRotate(Node<T>* n) {
+    Node<T>* nRight = n->right;
+    nRight->parent = n->parent;
+    if (n->parent == NULL)
+        root = nRight;
+    else {
+        if (n == n->parent->left)
+            n->parent->left = nRight;
+        else
+            n->parent->right = nRight;
+    }
+    n->parent = nRight;
+    n->right = nRight->left;
+    if (n->right != NULL)
+        n->right->parent = n;
+    nRight->left = n;
+}
 
+
+template<typename T, typename F>
+void Set<T, F>::rightRotate(Node<T>* n) {
+    Node<T>* nLeft = n->left;
+    nLeft->parent = n->parent;
+    if (nLeft->parent == NULL)
+        root = nLeft;
+    else {
+        if (n == n->parent->left)
+            n->parent->left = nLeft;
+        else
+            n->parent->right = nLeft;
+    }
+    n->parent = nLeft;
+    n->left = nLeft->right;
+    if (n->left != NULL)
+        n->left->parent = n;
+    nLeft->right = n;
+}
+
+template<typename T, typename F>
+void Set<T, F>::fixInsert(Node<T>* newNode) {
+    while (newNode != root && newNode->color == 'r' && newNode->parent->color == 'r') {
+        Node<T>* parentNode = newNode->parent;
+        Node<T>* grandParentNode = parentNode->parent;
+        Node<T>* uncleNode = NULL;
+
+        //Left
+        if (parentNode == grandParentNode->left) {
+            uncleNode = grandParentNode->right;
+            if (uncleNode != NULL && uncleNode->color == 'r') {
+                grandParentNode->color = 'r';
+                parentNode->color = 'b';
+                uncleNode->color = 'b';
+                newNode = grandParentNode;
+            }
+            else {
+                //Left-Right 
+                if (newNode == parentNode->right) {
+                    newNode = parentNode;
+                    leftRotate(newNode);
+                }
+                rightRotate(grandParentNode);
+                newNode->parent->color = 'b';
+                grandParentNode->color = 'r';
+            }
+        }
+
+        //Right
+        else {
+            uncleNode = grandParentNode->left;
+            if (uncleNode != NULL && uncleNode->color == 'r') {
+                grandParentNode->color = 'r';
+                parentNode->color = 'b';
+                uncleNode->color = 'b';
+                newNode = grandParentNode;
+            }
+            else {
+                //Right-Left
+                if (newNode == parentNode->left) {
+                    newNode = parentNode;
+                    rightRotate(newNode);
+                }
+                leftRotate(grandParentNode);
+                newNode->parent->color = 'b';
+                grandParentNode->color = 'r';
+            }
+        }
+    }
+    root->color = 'b';
 }
 
 
